b51a6bd8ab1886428830ed38049613dd3deb4411
wh1t3h47 <tom.mharres@gmail.com>
2021-06-18 15:30:17 -0300

    separar definiçoes

---
+++ src/definitions.js
@@ -0,0 +1,36 @@
+/** @type {{ Nome: string, DataInicio: string | Date, DataFim: string | Date }} */
+const colecao_signos = [
+  { Nome: 'Aquário',
+    DataInicio: '01-20', DataFim: '02-18'},
+  { Nome: 'Peixes',
+    DataInicio: '02-19', DataFim: '03-20'},
+  { Nome: 'Áries',
+    DataInicio: '03-21', DataFim: '04-19'},
+  { Nome: 'Touro',
+    DataInicio: '04-20', DataFim: '05-20'},
+  { Nome: 'Gêmeos',
+    DataInicio: '05-21', DataFim: '06-20'},
+  { Nome: 'Câncer',
+    DataInicio: '06-21', DataFim: '07-22'},
+  { Nome: 'Leão',
+    DataInicio: '07-23', DataFim: '08-22'},
+  { Nome: 'Virgem',
+    DataInicio: '08-23', DataFim: '09-22'},
+  { Nome: 'Libra',
+    DataInicio: '09-23', DataFim: '10-22'},
+  { Nome: 'Escorpião',
+    DataInicio: '10-23', DataFim: '11-21'},
+  { Nome: 'Sagitário',
+    DataInicio: '11-22', DataFim: '12-21'},
+  { Nome: 'Capricórnio',
+    DataInicio: '12-22', DataFim: '01-19'},
+];
+
+/** enum */
+const or = 0;
+/** enum */
+const and = 1;
+/** @type {Record<'and' | 'or', number>} */
+const operacao = { and, or };
+  
+module.exports = { colecao_signos, operacao }
