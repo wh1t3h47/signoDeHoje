d5794fe841c213e19220bd27962ab21cafc5360a
wh1t3h47 <tom.mharres@gmail.com>
2021-06-23 02:45:27 -0300

    Mais testes (descobri 8 signos incorret0s)

--- src/signoDeHoje.test.js
+++ src/signoDeHoje.test.js
@@ -1,85 +1,42 @@
 const retorna_signo = require("./signoDeHoje").default;
-const { colecao_signos } = require("./definitions");
 
 // Sou meio cético, mas vai lá, pelo esporte mesmo...
 /** @typedef {'Aquário'|'Peixes'|'Áries'|'Touro'|'Gêmeos'|'Câncer'|'Leão'|'Virgem'|'Libra'|'Escorpião'|'Sagitário'|'Capricórnio'} signoComComplecaoVsCode */
 
-test('Testar pessoa de Touro (o desenvolvedor)', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Touro"
-    expect(retorna_signo(new Date('05-12-2000'))).toMatch(signo)
-});
+/**
+ * @param {string} testName
+ * @param {Date} data 
+ * @param {signoComComplecaoVsCode} signo 
+ **/
+const testarSigno = (testName, data, signo) => {
+    test(`${testName} | data = ${data.toUTCString()}`, () => {
+      expect(retorna_signo(data)).toMatch(signo);
+    });
+}
 
 
-test('Testar pessoa de Escorpião', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Escorpião"
-    expect(retorna_signo(new Date('10-24-1997'))).toMatch(signo)
-})
+testarSigno('Testar pessoa de Touro (o desenvolvedor)', new Date('05-12-2000'), "Touro")
 
-test('Testar pessoa virgem', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Virgem"
-    expect(retorna_signo(new Date('08-23-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa de Escorpião', new Date('10-24-1997'), "Escorpião")
 
-test('Testar pessoa virgem', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Virgem"
-    expect(retorna_signo(new Date('09-22-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa virgem', new Date('08-23-2005'), "Virgem")
 
-test('Testar pessoa aquário', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Aquário"
-    expect(retorna_signo(new Date('01-23-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa virgem', new Date('09-22-2005'), "Virgem")
 
-test('Testar pessoa peixe', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Peixes"
-    expect(retorna_signo(new Date('02-21-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa aquário', new Date('01-23-2005'), "Aquário")
 
-test('Testar pessoa aries', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Áries"
-    expect(retorna_signo(new Date('04-19-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa peixe', new Date('02-21-2005'), "Peixes")
 
-test('Testar pessoas gemeas', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Gêmeos"
-    expect(retorna_signo(new Date('05-25-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoa aries', new Date('04-19-2005'), "Áries")
 
-test('Testar pessoas de cancer', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Câncer"
-    expect(retorna_signo(new Date('06-24-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoas gemeas', new Date('05-25-2005'), "Gêmeos")
 
-test('testar leão selvagem', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Leão"
-    expect(retorna_signo(new Date('07-29-2005'))).toMatch(signo)
-})
+testarSigno('Testar pessoas de cancer', new Date('06-24-2005'), "Câncer")
 
-test('testar moeda libra', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Libra"
-    expect(retorna_signo(new Date('10-22-2005'))).toMatch(signo)
-})
+testarSigno('testar leão selvagem', new Date('07-29-2005'), "Leão")
 
-test('testar pessoa Capricornoano', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Capricórnio"
-    expect(retorna_signo(new Date('01-20-2005'))).toMatch(signo)
-})
+testarSigno('testar moeda libra', new Date('10-22-2005'), "Libra")
 
-test('testar pessoa sagiotário', () => {
-    /** @type {signoComComplecaoVsCode} */
-    const signo = "Sagitário"
-    expect(retorna_signo(new Date('12-18-2005'))).toMatch(signo)
-})
+testarSigno('testar pessoa Capricornoano', new Date('01-20-2005'), "Capricórnio")
 
+testarSigno('testar pessoa sagiotário', new Date('12-18-2005'), "Sagitário")
---
+++ src/signoDeHojeOffByOne.test.js
@@ -0,0 +1,67 @@
+const retorna_signo = require("./signoDeHoje").default;
+
+// Sou meio cético, mas vai lá, pelo esporte mesmo...
+/** @typedef {'Aquário'|'Peixes'|'Áries'|'Touro'|'Gêmeos'|'Câncer'|'Leão'|'Virgem'|'Libra'|'Escorpião'|'Sagitário'|'Capricórnio'} signoComComplecaoVsCode */
+
+// Sou meio cético, mas vai lá, pelo esporte mesmo...
+/** @typedef {'Aquário'|'Peixes'|'Áries'|'Touro'|'Gêmeos'|'Câncer'|'Leão'|'Virgem'|'Libra'|'Escorpião'|'Sagitário'|'Capricórnio'} signoComComplecaoVsCode */
+
+/**
+ * @param {string} testName
+ * @param {Date} data 
+ * @param {signoComComplecaoVsCode} signo 
+ **/
+ const testarSigno = (testName, data, signo) => {
+    test(`${testName} | data = ${data.toUTCString()}`, () => {
+      expect(retorna_signo(data)).toMatch(signo);
+    });
+}
+
+
+testarSigno('testar primeiro dia de Aquário)', new Date('01-21-2000'), "Aquário")
+
+testarSigno('testar último dia de Aquário)', new Date('02-19-2000'), "Aquário")
+
+testarSigno('testar primeiro dia de Peixes)', new Date('02-20-2000'), "Peixes")
+
+testarSigno('testar último dia de Peixes)', new Date('03-20-2000'), "Peixes")
+
+testarSigno('testar primeiro dia de Áries)', new Date('03-21-2000'), "Áries")
+
+testarSigno('testar último dia de Áries)', new Date('04-20-2000'), "Áries")
+
+testarSigno('testar primeiro dia de Touro)', new Date('03-21-2000'), "Touro")
+
+testarSigno('testar último dia de Touro)', new Date('05-20-2000'), "Touro")
+
+testarSigno('testar primeiro dia de Gêmeos)', new Date('05-21-2000'), "Gêmeos")
+
+testarSigno('testar último dia de Gêmeos)', new Date('06-20-2000'), "Gêmeos")
+
+testarSigno('testar primeiro dia de Câncer)', new Date('06-21-2000'), "Câncer")
+
+testarSigno('testar último dia de Câncer)', new Date('07-21-2000'), "Câncer")
+
+testarSigno('testar primeiro dia de Leão)', new Date('07-22-2000'), "Leão")
+
+testarSigno('testar último dia de Leão)', new Date('08-22-2000'), "Leão")
+
+testarSigno('testar primeiro dia de gente Virgem)', new Date('08-23-2000'), "Virgem")
+
+testarSigno('testar último dia de gente Virgem)', new Date('08-22-2000'), "Virgem")
+
+testarSigno('testar primeiro dia de Libra)', new Date('08-23-2000'), "Libra")
+
+testarSigno('testar último dia de Libra)', new Date('10-22-2000'), "Libra")
+
+testarSigno('testar primeiro dia de Escorpião)', new Date('10-23-2000'), "Escorpião")
+
+testarSigno('testar último dia de Escorpião)', new Date('11-21-2000'), "Escorpião")
+
+testarSigno('testar primeiro dia de Sagitário)', new Date('11-22-2000'), "Sagitário")
+
+testarSigno('testar último dia de Sagitário)', new Date('12-21-2000'), "Sagitário")
+
+testarSigno('testar primeiro dia de Capricórnio)', new Date('12-22-2000'), "Capricórnio")
+
+testarSigno('testar último dia de Capricórnio)', new Date('01-20-2000'), "Capricórnio")
